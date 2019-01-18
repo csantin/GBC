a865ebf85fcc4b3962fb1731f25362cb43ce62bc
Christopher Santin <101076623@georgebrown.ca>
2019-01-18 16:31:01 -0500

    Lab 2

---
+++ lab2/ex6.js
@@ -0,0 +1,5 @@
+const car2 = new car2('Pontiac Firebird', 1976);
+console.log(car2.details());
+// Subclass - extends Car super class
+const sedan = new Sedan('Volvo SD', 2018, 30000);
+console.log(sedan.info());
